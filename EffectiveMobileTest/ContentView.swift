77b449390ac74bf5844c5995549ebaa502bed3c9
Iuliia Vlkv <yu.wlkw@gmail.com>
2024-05-29 15:39:17 +0500

    Initial Commit

---
+++ EffectiveMobileTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EffectiveMobileTest
+//
+//  Created by Iuliia Volkova on 29.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Iuliia Volkova on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
